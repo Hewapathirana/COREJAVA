ac56f8974af06cf6e210858697310f88ce7b1b38
Hewapathirana <dasisandaruwan@gmail.com>
2020-06-23 08:34:44 +0530

    used lambda expression for implemnt anonymous class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-      Runnable vehicle = new Vehicle();
-      Runnable car = new Car();
+
+        //here we are using a anonymous class to implement the runnable functional interface
+      Runnable vehicle = () -> {
+              for(int i =1;i<6;i++) {
+                  System.out.println("Vehicle");
+                  try { Thread.sleep(500);} catch (Exception e){};
+              }
+          };
+
+      Runnable car = () ->{
+          for(int i =1;i<6;i++) {
+              System.out.println("Car");
+              try { Thread.sleep(500);} catch (Exception e){};
+          }
+      };
+
+      // we can minimize the code by replace the vehicle with above lambda expression
 
       Thread t1 = new Thread(vehicle);
       Thread t2 = new Thread(car);
